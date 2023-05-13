cd6caa0b4fae4a6c0f217bc3daa5a1d768e50778
Ryan <ryan@trat.io>
2023-05-13 11:51:28 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -9,6 +9,6 @@ IT IS ALSO RECCOMENDED YOU DO NOT STORE THESE KEYS SERVER SIDE, AND INSTEAD USE 
 */
 
 export const openai_config = new Configuration({
-  apiKey: "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",
+  apiKey: "",
   organization: "org-OgDNtml2WVkO1nAGoM6Z2CHl",
 });
