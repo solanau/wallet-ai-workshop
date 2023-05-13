d2b9684d1d1f3d3d1f704f6296dda3b4e378f0d0
Ryan <ryan@trat.io>
2023-05-13 13:37:13 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -6,9 +6,11 @@ THESE KEYS ARE FOR WORKSHOP USE ONLY. THEY WILL BE DELETED AFTER THE WORKSHOP.
 
 IT IS ALSO RECCOMENDED YOU DO NOT STORE THESE KEYS SERVER SIDE, AND INSTEAD USE SERVER SIDE CODE TO EXECUTE OPENAI CALLS.
 
+
+// KEYS HAVE BEEN REMOVED AND DELETED. get your own key at platform.openai.com
 */
 
 export const openai_config = new Configuration({
-  apiKey: "sk-" + "tlXx39KnoSz4uOLC7Zoo" + "T3BlbkFJcO7047M3O7qG7QeTFojm",
-  organization: "org-OgDNtml2WVkO1nAGoM6Z2CHl",
+  apiKey: "",
+  organization: "",
 });
